b6add9d7b4f9522ca15ce99817b21274c74d41e5
Leila <leila0777@icloud.com>
2022-04-30 18:11:20 +0300

    Initial Commit

---
+++ Udemy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udemy
+//
+//  Created by leila's mac on 30.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
